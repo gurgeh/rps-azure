cb32d2241338429d7013e8c9a9b479feee5e2bbb
David Fendrich <david@fendrich.se>
2016-02-26 17:29:36 +0100

    Removed empty columns for first training

--- src/prepare_history.py
+++ src/prepare_history.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 import csv
-
 """
 + kön
 + IT/verksamhet-slider (mest på skoj)
@@ -12,11 +11,14 @@ openings.txt verkar ha format (både från kod och issue)
 history:player history:computer playerplayed: R S P
 """
 
-HEADER_NAMES = ['gender', 'it', 'r_perc', 'p_perc', 's_perc', 'doubles', 'tripples', 'lose_stay', 'lose_copy', 'lose_beat_copy', 'win_stay', 'win_copy', 'win_beat_copy', 'draw_stay', 'draw_copy', 'draw_beat_copy']
-HISTORY_NAMES = ['h1', 'c1', 'h2', 'c2', 'h3', 'c3', 'h4', 'c4', 'h5', 'c5', 'label']
+HEADER_NAMES = [
+]  # ['gender', 'it', 'r_perc', 'p_perc', 's_perc', 'doubles', 'tripples', 'lose_stay', 'lose_copy', 'lose_beat_copy', 'win_stay', 'win_copy', 'win_beat_copy', 'draw_stay', 'draw_copy', 'draw_beat_copy']
+HISTORY_NAMES = ['h1', 'c1', 'h2', 'c2', 'h3', 'c3', 'h4', 'c4', 'h5', 'c5',
+                 'label']
 EXTRA_HEADERS = [''] * len(HEADER_NAMES)
 HISTORY = '../data/openings.txt'
 OUTPUT = '../data/history.csv'
+
 
 def parse_line(s):
     player_hist, computer_hist, R, S, P = s.split()
@@ -34,10 +36,10 @@ def make_features(ph, ch, R, P, S):
         history[n * 2] = ph[-(1 + n):]
         history[n * 2 + 1] = ch[-(1 + n):]
 
-
     for nr, label in [(R, 0), (P, 1), (S, 2)]:
         for _ in range(nr):
             yield history + [label]
+
 
 def convert(inname=HISTORY, outname=OUTPUT):
     with open(inname) as inf:
@@ -48,6 +50,7 @@ def convert(inname=HISTORY, outname=OUTPUT):
                 if s.strip():
                     for row in make_features(*parse_line(s)):
                         cout.writerow(EXTRA_HEADERS + row)
+
 
 if __name__ == '__main__':
     convert()
