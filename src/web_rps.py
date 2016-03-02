190cfa9501af0353287e4455951155990d1043e5
David Fendrich <david@fendrich.se>
2016-03-02 14:25:41 +0100

    Error assertion and bug fix

--- src/web_rps.py
+++ src/web_rps.py
@@ -4,7 +4,12 @@ import rps_game
 app = Flask(__name__)
 
 
-@app.route("/move/<player>/<computer>")
+@app.route("/move/<player>/<computer>/")
 def get_move(player, computer):
-    rg = rps_game()
-    return jsonify(move=rg.set_history(player, computer))
+    rg = rps_game.RPSGame()
+    assert len(player.strip()) == len(computer.strip())
+    return jsonify(move=rg.set_history(player.strip(), computer.strip()))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
