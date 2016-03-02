4b0be0ce232d504176731dc588cf0dffd033ed7e
David Fendrich <david@fendrich.se>
2016-03-02 13:33:45 +0100

    Added files for Azure scaffolding

---
+++ runserver.py
@@ -0,0 +1,4 @@
+import src.flask_test
+
+src.flask_test.MESSAGE = "Hello from runserver!"
+src.flask_test.app.run()
--- src/flask_test.py
+++ src/flask_test.py
@@ -1,10 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+MESSAGE = "Hello World!"
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return MESSAGE
 
 
 if __name__ == "__main__":
