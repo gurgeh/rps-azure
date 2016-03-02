7e8b06f82de9112fc0eedaef83b3e3006365d721
David Fendrich <david@fendrich.se>
2016-03-02 17:01:43 +0100

    Started client

---
+++ static/js/rps.js
@@ -0,0 +1,20 @@
+var human_wins;
+var computer_wins;
+
+var human_history;
+var computer_history;
+
+var next_move;
+
+function initialize(){
+    human_wins = 0
+    computer_wins = 0
+    human_history = ''
+    computer_wins = ''
+}
+
+function get_next_move(){
+    //prepare history
+    //check jquery
+}
+
