85e2f260871e3f94518c6e898c6722629b951c97
David Fendrich <david@fendrich.se>
2016-03-03 15:56:14 +0100

    Working semipretty game

--- src/web_rps.py
+++ src/web_rps.py
@@ -1,8 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory, send_file
 import rps_game
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
+@app.route("/")
+def index():
+    return send_file('static/game.html', 'text/html')
 
 @app.route("/move/<player>/<computer>/")
 def get_move(player, computer):
@@ -10,6 +13,9 @@ def get_move(player, computer):
     assert len(player.strip()) == len(computer.strip())
     return jsonify(move=rg.set_history(player.strip(), computer.strip()))
 
+@app.route("/static/<path:path>")
+def send_static(path):
+    return send_from_directory('static', path)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/rps.js
+++ static/js/rps.js
@@ -1,16 +1,11 @@
 /*
-flip sign
-add images for pick
-highlight pick
-highlight winner
-highlight draw
-
 --prettify
-columns
-transparent countdown over signs
-Larger current score + maybe colors
-Help text
+push and verify
+
+--more pretty
+maybe graphs
 Title
+More messages?
 */
 
 var human_wins;
@@ -24,75 +19,102 @@ var computer_history;
 var next_move;
 var human_move;
 
-MOVE_LETTER = ['R', 'P', 'S']
+var frozen_human;
+var frozen_computer;
 
-BASESIGN = '/static/images/600px-'
-SIGNS = ["rock", "paper", 'scissors']
+MOVE_LETTER = ['R', 'P', 'S'];
 
-function show_signs(human, computer){
-    document.getElementById('human_sign').src = BASESIGN + SIGNS[human] + '.png'
-    document.getElementById('computer_sign').src = BASESIGN + SIGNS[computer] + '.png'
+BASESIGN = '/static/images/600px-';
+SIGNS = ["rock", "paper", 'scissors'];
+
+function show_signs(){
+    hs = document.getElementById('human_sign');
+    cs = document.getElementById('computer_sign');
+
+    hs.src = BASESIGN + SIGNS[frozen_human] + '.png';
+    cs.src = BASESIGN + SIGNS[frozen_computer] + '.png';
+}
+
+function hide_signs(){
+    document.getElementById('human_sign').src="/static/images/1x1.gif";
+    document.getElementById('computer_sign').src="/static/images/1x1.gif";
+    setTimeout(show_signs, 300);
 }
 
 function show_score(){
-    document.getElementById('human_wins').innerHTML = human_wins
-    document.getElementById('draws').innerHTML = draws
-    document.getElementById('computer_wins').innerHTML = computer_wins
+    document.getElementById('human_wins').innerHTML = human_wins;
+    document.getElementById('draws').innerHTML = draws;
+    document.getElementById('computer_wins').innerHTML = computer_wins;
+}
+
+function message(s){
+    document.getElementById("message").innerHTML = s;
 }
 
 function initialize(){
-    document.getElementById("message").innerHTML = 'First to 20 wins!'
-    human_wins = 0
-    computer_wins = 0
+    message('Choose a symbol below! Quickly!');
+    document.getElementById("score").style.visibility = 'visible';
+    human_wins = 0;
+    computer_wins = 0;
     draws = 0;
-    human_history = ''
-    computer_history = ''
-    
-    get_next_move()
+    human_history = '';
+    computer_history = '';
+
+    get_next_move();
 }
 
 function draw(){
     draws++;
+    document.getElementById('human_sign').style.backgroundColor = 'white';
+    document.getElementById('computer_sign').style.backgroundColor = 'white';
 }
 
 function hwin(){
     human_wins++;
+    document.getElementById('human_sign').style.backgroundColor = '#7ddcdc';
+    document.getElementById('computer_sign').style.backgroundColor = '#fe465b';
 }
 
 function hlose(){
     computer_wins++;
+    document.getElementById('human_sign').style.backgroundColor = '#fe465b';
+    document.getElementById('computer_sign').style.backgroundColor = '#7ddcdc';
 }
 
 function pick_move(n){
     human_move = n;
     function cset(n, col){
         e = document.getElementById('pickmove'+n);
-        e.style.color = col;
+        e.style.backgroundColor = col;
     }
-    cset(0, "black")
-    cset(1, "black")
-    cset(2, "black")
-    cset(n, "red")
+    cset(0, "white");
+    cset(1, "white");
+    cset(2, "white");
+    cset(n, "#eae5da");
 
     if(nrgames==0){
         initialize();
         decide();
+    } else if(nrgames < 10) {
+        message('First to 20 wins! It takes me a while to get started..');
+    } else {
+        message('First to 20 wins!');
     }
 }
 
 function get_next_move(){
     if(human_history == ''){
         next_move = Math.floor(Math.random() * 3);
-        return
+        return;
     }
     next_move = -1;
 
     var xmlhttp = new XMLHttpRequest();
-    var url = "http://advectas-rps.azurewebsites.net/move/"+human_history+"/"+computer_history+"/";
-    
+    var url = "/move/"+human_history+"/"+computer_history+"/";
+
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            next_move = JSON.parse(xmlhttp.responseText);
+            next_move = JSON.parse(xmlhttp.responseText)['move'];
         }
     };
     xmlhttp.open("GET", url, true);
@@ -101,31 +123,33 @@ function get_next_move(){
 
 function decide(){
     if(next_move == -1) {
-        setInterval(decide, 1000)
+        setTimeout(decide, 1000);
     } else {
         nrgames++;
-        show_signs(human_move, next_move)
+        hide_signs();
+        frozen_human = human_move;
+        frozen_computer = next_move;
         if(next_move==0){
-            if(human_move==0) draw()
-            if(human_move==1) hwin()
-            if(human_move==2) hlose()
+            if(human_move==0) draw();
+            if(human_move==1) hwin();
+            if(human_move==2) hlose();
         } else if(next_move==1){
-            if(human_move==1) draw()
-            if(human_move==2) hwin()
-            if(human_move==0) hlose()
+            if(human_move==1) draw();
+            if(human_move==2) hwin();
+            if(human_move==0) hlose();
         } else {
-            if(human_move==2) draw()
-            if(human_move==0) hwin()
-            if(human_move==1) hlose()
+            if(human_move==2) draw();
+            if(human_move==0) hwin();
+            if(human_move==1) hlose();
         }
         show_score();
         if(human_wins==20){
-            document.getElementById("message").innerHTML = 'You won! Lucky humans.. -> Reload page to play again.'
+            document.getElementById("message").innerHTML = 'You won! Lucky humans.. -> Reload page to play again.';
         } else if(computer_wins==20) {
-            document.getElementById("message").innerHTML = 'I win again! Predictable humans.. -> Reload page to play again.'
+            document.getElementById("message").innerHTML = 'I win again! Predictable humans.. -> Reload page to play again.';
         } else {
-            human_history += MOVE_LETTER[human_move]
-            computer_history += MOVE_LETTER[next_move]
+            human_history += MOVE_LETTER[human_move];
+            computer_history += MOVE_LETTER[next_move];
             get_next_move();
             start_countdown();
         }
@@ -142,9 +166,9 @@ function Countdown(options) {
 
   function decrementCounter() {
     updateStatus(seconds);
-    if (seconds === 0) {
-      counterEnd();
+      if (seconds <= 0) {
       instance.stop();
+      counterEnd();
     }
     seconds--;
   }
@@ -163,11 +187,14 @@ function Countdown(options) {
 
 
 function start_countdown(){
-    var myCounter = new Countdown({  
+    var myCounter = new Countdown({
         seconds:5,  // number of seconds to count down
-        onUpdateStatus: function(sec){document.getElementById('counter').innerHTML=''+sec}, // callback for each second
-        onCounterEnd: function(){ decide() }
+        onUpdateStatus: function(sec){
+            c = document.getElementById('counter');
+            if(sec > 0) c.innerHTML = '' + sec;
+            else c.innerHTML = '';
+        }, // callback for each second
+        onCounterEnd: function(){ decide(); }
     });
     myCounter.start();
 }
-
