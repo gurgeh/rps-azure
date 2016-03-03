3531feb83dc3e4adb641744741521bcf30fd3f5e
David Fendrich <david@fendrich.se>
2016-03-03 15:59:27 +0100

    Make paths work on window

--- src/web_rps.py
+++ src/web_rps.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, send_from_directory, send_file
 import rps_game
 
@@ -5,7 +6,7 @@ app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def index():
-    return send_file('static/game.html', 'text/html')
+    return send_file(os.path.join('static', 'game.html'), 'text/html')
 
 @app.route("/move/<player>/<computer>/")
 def get_move(player, computer):
