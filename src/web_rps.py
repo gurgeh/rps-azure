0fabdde908c24d5a139d4eed25e91f84800606a3
David Fendrich <david@fendrich.se>
2016-03-03 16:04:17 +0100

    redirect seems more stable

--- src/web_rps.py
+++ src/web_rps.py
@@ -1,12 +1,11 @@
-import os
-from flask import Flask, jsonify, send_from_directory, send_file
+from flask import Flask, jsonify, send_from_directory, redirect
 import rps_game
 
 app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def index():
-    return send_file(os.path.join('static', 'game.html'), 'text/html')
+    return redirect('/static/game.html')
 
 @app.route("/move/<player>/<computer>/")
 def get_move(player, computer):
